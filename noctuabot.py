09687265ed1171a138a4851b50fbd8446fe8b755
Admin <Admin@AdmintekiMacBook-Pro.local>
2017-06-09 16:24:33 +0800

    support photos

--- noctuabot.py
+++ noctuabot.py
@@ -292,13 +292,15 @@ class User:
 
     def admin(self,text,chat,name):
         if text == "/view":
-            items = db.get_all()
+            items = db.get_Suggestions()
+            items += db.get_General()
             hold = ["("+x[2]+")"+" "+x[4]+": "+x[1] for x in items]
             return [str(i+1) + ". " + x for i, x in enumerate(hold)]
             message = "\n".join(items)
             send_message(message, chat, remove_keyboard())
         elif text == "/delete":
-            items = db.get_all()
+            items = db.get_Suggestions()
+            items += db.get_General()
             hold = ["("+x[2]+")"+" "+x[4]+": "+x[1] for x in self.cur]
             return [str(i+1) + ". " + x for i, x in enumerate(hold)]
             message = "\n".join(items)
@@ -340,7 +342,8 @@ class User:
     def delete(self,text,chat,name):
         if text != "back":
             try:
-                items = db.get_all()
+                items = db.get_Suggestions()
+                items += db.get_General()
                 index = int(text) - 1
                 feedback = items[index][1]
                 db.delete_item(feedback)
