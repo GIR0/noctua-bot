71a06691b0f26f5c97fe663a31fd1bc1dee63d63
Admin <Admin@AdmintekiMacBook-Pro.local>
2017-06-13 18:06:50 +0800

    RateEvents

--- noctuabot.py
+++ noctuabot.py
@@ -410,9 +410,7 @@ class User:
         elif text in events:
             ratings = [x[6]+"\n"+x[2]+"\n"+x[3]+"\n"+x[4]+ " " for x in event.get_by_event(text)]
             ratings = [str(i+1) + ". " + x for i, x in enumerate(ratings)]
-            print ratings
-            message = text + "\n\n" + "\n\n".join(ratings)
-            print message
+            message = "\n\n".join(ratings)
             send_message(message, chat, remove_keyboard())
         self.stage = self.admin
 
