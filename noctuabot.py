45610e9de0b4c3a5a97bbe6e5b7f262cbaf77572
Admin <Admin@AdmintekiMacBook-Pro.local>
2017-06-15 15:26:12 +0800

    RateEvents

--- noctuabot.py
+++ noctuabot.py
@@ -130,7 +130,7 @@ class User:
         self.event = ""
     def MainMenu(self,text,chat,name):
         print(type(text))
-        print text
+        print repr(text)
         print(type(u"About the Bot\ud83e\udd89"))
         if chat in admin:
             if text == "/admin":
