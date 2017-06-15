6eaf7c703de4838c4fd7bafa3d79628015f73b92
Admin <Admin@AdmintekiMacBook-Pro.local>
2017-06-15 14:50:00 +0800

    RateEvents

--- noctuabot.py
+++ noctuabot.py
@@ -154,7 +154,7 @@ class User:
             keyboard = build_keyboard(options)
             send_message("Which event would you like to rate?", chat, keyboard)
             self.stage = self.rate1
-        elif text == urllib.quote_plus(u"About the Bot\ud83e\udd89".encode("utf8")):
+        elif text == urllib.quote_plus((u"About the Bot\ud83e\udd89").encode("utf8")):
             options =[("back")]
             keyboard = build_keyboard(options)
             send_message("It's just a BOT :)", chat, keyboard)
