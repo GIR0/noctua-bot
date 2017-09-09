1fe24abdb5bb9632434176c351035d9a2b7f9bf6
Admin <Admin@AdmintekiMacBook-Pro.local>
2017-09-10 00:21:33 +0800

    Improvements

--- noctuabot.py
+++ noctuabot.py
@@ -1474,7 +1474,7 @@ class User:
                     message += key + ": " + str(stats[key]) + "\n"
                     options.append([{"text":key, "callback_data":key}])
                 options = sorted(options, key=str.lower)
-                query_results.append({"type": "article", "id": x[:20], "title": x, "input_message_content": {"message_text": x + "\n\n" + message}, "reply_markup": options})
+                query_results.append({"type": "article", "id": x[:20], "title": x, "input_message_content": {"message_text": x + "\n\n" + message}, "reply_markup": {"inline_keyboard":options}})
                 print message
             query_results = json.dumps(query_results)
             answer_inline_query(query_id, query_results)
